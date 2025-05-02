946baa995c450f0eb45b0eaa571c234079c72d7d
PalavroDev <gustavopalavro725@gmail.com>
2025-05-02 09:48:36 -0300

    Cores alteradas

--- html-css/js/script.js
+++ html-css/js/script.js
@@ -1,15 +1,15 @@
     // Ao clicar o container roda
-document.getElementById('showSignupBtn').addEventListener('click', function() {
-    document.getElementById('containerWrapper').style.transform = 'rotateY(180deg)';
-  });
-    // Ao clicar o container roda
-  document.getElementById('showLoginBtn').addEventListener('click', function(e) {
-    e.preventDefault();
-    document.getElementById('containerWrapper').style.transform = 'rotateY(0deg)';
-  });
-
-
-    // Seleciona todos os campos do formulário e limpa os valores
-  function limparCampos() {
-    document.getElementById("signup Form").reset();
-}
+    document.getElementById('btCadastro').addEventListener('click', function() {
+      document.getElementById('containerWrapper').style.transform = 'rotateY(180deg)';
+    });
+      // Ao clicar o container roda
+    document.getElementById('btLogin').addEventListener('click', function(e) {
+      e.preventDefault();
+      document.getElementById('containerWrapper').style.transform = 'rotateY(0deg)';
+    });
+  
+  
+      // Seleciona todos os campos do formulário e limpa os valores
+    function limparCampos() {
+      document.getElementById("signup Form").reset();
+  }
