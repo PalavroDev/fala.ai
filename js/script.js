83709989c6262e676ffca47b0d2b962d305aeb0b
Gustavo Palavro <gustavopalavro725@gmail.com>
2025-05-09 13:43:22 -0300

    Mudanças na tela de login e principal
    mais campos em tela de login, e mais conteudo na index

--- js/script.js
+++ js/script.js
@@ -13,3 +13,15 @@
     function limparCampos() {
       document.getElementById("signup Form").reset();
   }
+
+  function mostrarTexto(botao) {
+    const texto = document.getElementById("texto");
+
+    if (botao === 1) {
+      texto.innerText = "Garçons anotam pedidos em dispositivos que enviam direto à cozinha. Reduzem erros de anotação e aumentam a velocidade do atendimento.";
+    } else if (botao === 2) {
+      texto.innerText = "Centraliza pedidos, pagamentos, controle de caixa e emissão de notas fiscais. Otimiza o fechamento do dia e reduz falhas humanas.";
+    } else if (botao === 3) {
+      texto.innerText = "Tela na cozinha que exibe os pedidos em tempo real. Elimina o uso de papéis e melhora a organização do preparo.";
+    }
+  }
